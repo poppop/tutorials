3bc1de0d2ff150e2956cb221416c86db354bd8f3
ekohn8 <erickohn8@gmail.com>
2017-11-08 12:44:27 -0500

    Update config.js

--- express-db/config.js
+++ express-db/config.js
@@ -1,9 +1,9 @@
 module.exports = {
     db: {
-        host: 'ec2-54-163-255-181.compute-1.amazonaws.com',
+        host: '',
         port: 5432,
-        user: 'olwyysvxeikadm',
-        password: 'c276873b5715b0f8b1c4e8b8dad0687f6e27edd27bcf1d8ba971c64f754d9647',
-        database: 'd2dkkns7b2b126',
+        user: '',
+        password: '',
+        database: '',
     }
 }
